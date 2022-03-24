c1053d61d94c5c829f3755f559018c94db973751
MalkareddyJithender <jithendermalkareddy@gmail.com>
2022-03-24 15:59:13 +0530

    removed cors additional code

--- src/app.js
+++ src/app.js
@@ -6,19 +6,12 @@ const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task');
 const cors = require('cors');
 
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-
 
 const app = express();
 
-
+app.use(cors());
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter);
-app.use(cors(corsOptions));
 
 module.exports = app
