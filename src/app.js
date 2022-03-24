6d5da32cdfc2cd0bd43014707b560685cbdf970a
MalkareddyJithender <jithendermalkareddy@gmail.com>
2022-03-24 14:36:57 +0530

    cors middleware added

--- src/app.js
+++ src/app.js
@@ -3,14 +3,16 @@ require('./db/mongoose')
 const User = require('./models/user')
 const Task = require('./models/task')
 const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
+const taskRouter = require('./routers/task');
+const cors = require('cors');
 
 
-const app = express()
+const app = express();
 
 
 app.use(express.json())
 app.use(userRouter)
-app.use(taskRouter)
+app.use(taskRouter);
+app.use(cors());
 
 module.exports = app
